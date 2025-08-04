58b8c58ea592378483c0f308827f809ad9e83779
Chris Arridge <chris.arridge@opendataservices.coop>
2025-08-04 11:25:25 +0100

    chore: setup environment variables and secret key generation
    Note, django-environ does not yet have type hints and so for the
    moment I have by-passed mypy type checking for this module.  This
    should be fixed in a more compliant way before moving into prod.

--- iati_account_web/settings.py
+++ iati_account_web/settings.py
@@ -10,20 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import secrets
 from pathlib import Path
+
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h22hx$rm#5qmpi#=s_^rdgu5_vz0m6!8)81n)mrrkkqq5i8&_7"
+# Setup environment variables.
+env = environ.Env(
+    DEBUG=(bool, False),
+    SECRET_KEY=(str, secrets.token_urlsafe(50)),
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS: list[str] = []
 
