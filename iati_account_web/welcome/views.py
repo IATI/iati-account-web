c903a69d465187e62f74f2355f375d0ea8befdc8
Chris Arridge <chris.arridge@opendataservices.coop>
2025-12-14 15:53:38 +0000

    feat: add user home page
    This updates the user home page to show helpful information and links.
    This closes https://github.com/IATI/iati-account-web/issues/40.

--- iati_account_web/welcome/views.py
+++ iati_account_web/welcome/views.py
@@ -1,14 +1,33 @@
+import logging
+
 from django.http import HttpRequest, HttpResponse
 from django.template import loader
+from iati_account_web.helpers import preflight_checks
+from iati_account_web.ryd_handling import RegisterYourDataSession
+from iati_account_web.ryd_handling.reporting_orgs import parse_org_list_to_objects
+
+audit_logger = logging.getLogger("audit")
 
 
 def index(request: HttpRequest) -> HttpResponse:
 
     if request.user.is_authenticated:
+        preflight = preflight_checks(request)
+        if not preflight.okay_to_continue:
+            return preflight.redirect
+
+        session = RegisterYourDataSession(request.session["oidc_access_token"], allow_redirects=True)
+        try:
+            response_json = session.get("/reporting-orgs")
+            org_list = parse_org_list_to_objects(response_json["data"], request.user.oidc_sub)
+        except Exception as exc:
+            audit_logger.error(f"Could not access RYD for user {request.user.oidc_sub} with error {exc}")
+            raise exc
+
+        context = {"orgs": org_list}
         template = loader.get_template("welcome/home.html")
     else:
+        context = {}
         template = loader.get_template("welcome/index.html")
-
-    context = {}
 
     return HttpResponse(template.render(context, request))
