1606a4265e3f6982701394d137910559f7278bad
Chris Arridge <chris.arridge@opendataservices.coop>
2026-03-04 10:53:19 +0000

    test: add mixin to force IATI OIDC login in Django test cases

--- iati_account_web/tests/iati_mock.py
+++ iati_account_web/tests/iati_mock.py
@@ -1,11 +1,13 @@
 import json
 import random
 import string
+import time
 import uuid
 from urllib.parse import parse_qs, urlparse
 
 import requests
 import responses
+from iati_account_web.account.models import IATIUser
 from iati_account_web.settings import COUNTRY_CODE_LOOKUP
 
 
@@ -150,3 +152,112 @@ def _generate_pagination(page: int, page_size: int, total_records: int, url: str
         pagination["links"]["prev"] = f"{url}?page={page-1}&page_size={page_size}"
 
     return pagination
+
+
+class ForceIatiLoginMixin:
+    """Mixin to provide forced IATI OIDC login for Django test cases
+
+    This mixin provides functionality to force login / perform session
+    manipulation for a Django TestCase to make it seem like a user has
+    logged in using OIDC via mozilla-django-oidc.  It creates an IATIUser
+    from a set of mocked claims, adds it to the Django database,
+    logins in the user to the Client, and then manipulates the session
+    so that SessionMiddleware will accept the login.
+
+    No functionality has yet been added to properly fake tokens (e.g.,
+    to send to the Register Your Data endpoints in IatiInfrastructureMock)
+    or set nonce/state values.
+    """
+
+    iati_oidc_claims = None
+    iati_oidc_access_token = "FAKED_ACCESS_TOKEN"  # nosec B105
+    iati_oidc_id_token = "FAKED_ID_TOKEN"  # nosec B105
+    iati_oidc_state = "FAKED_STATE"
+    iati_oidc_nonce = "FAKED_NONCE"
+    user = None
+
+    def create_iati_user_claims(
+        self,
+        unformatted_name: str = "Unit Test User",
+        email: str = "unittestuser@iatistandard.org",
+        has_been_onboarded: bool = True,
+        has_been_provisioned: bool = True,
+        has_ryd_role: bool = True,
+        iati_superadmin: bool = False,
+        exp_delta: int = 600,
+        registry_id: str = "",
+    ) -> None:
+        """Generates a set of mock user claims
+
+        Parameters
+        ----------
+        unformatted_name : str, optional
+            Unformatted name, by default "Unit Test User"
+        email : str, optional
+            Email address, by default "unittestuser@iatistandard.org"
+        has_been_onboarded : bool, optional
+            Has been onboarded into IATI, by default True
+        has_been_provisioned : bool, optional
+            User account has been provisioned, by default True
+        has_ryd_role : bool, optional
+            User has the IATI role to enable access to RYD, by default True
+        iati_superadmin : bool, optional
+            User has a superadmin role, by default False
+        exp_delta : int, optional
+            Offset for expiry of a related access/id token, by default 600 seconds
+        registry_id : str, optional
+            UUID for a matching RYD person record, by default ""
+
+        Returns
+        -------
+        dict[str, str | int | bool]
+            Dictionary of claims
+        """
+        now = int(time.time())
+        self.iati_oidc_claims = {
+            "sub": str(uuid.uuid4()),
+            "name": unformatted_name,
+            "email": email,
+            "iatiRegistryId": registry_id,
+            "iatiHasBeenOnboarded": "true" if has_been_onboarded else "false",
+            "iatiHasBeenProvisioned": "true" if has_been_provisioned else "false",
+            "roles": [],
+            "exp": now + exp_delta,
+            "iat": now,
+        }
+        if has_ryd_role:
+            self.iati_oidc_claims["roles"].append("Internal/iati_register_your_data")
+        if iati_superadmin:
+            self.iati_oidc_claims["roles"].append("Internal/iati_superadmin")
+
+    def create_user(self, **kwargs):
+        """Create a user object and add to Django
+
+        If claims have not already been created manually, this method will
+        generate them using create_iati_user_claims and for convenience will pass
+        any keyword arguments to this method.
+        """
+        if self.iati_oidc_claims is None:
+            self.create_iati_user_claims(**kwargs)
+        self.user = IATIUser.objects.create_user(
+            username=self.iati_oidc_claims["email"], email=self.iati_oidc_claims["email"]
+        )
+        self.user.update_from_claims(self.iati_oidc_claims, include_sub=True)
+        self.user.save()
+
+    def force_oidc_login(self):
+        """Force login by directly manipulating the session state
+
+        If no user or claims have been setup, these will be automatically
+        initialised by calling create_iati_user_claims() and create_user().
+        """
+        if self.user is None:
+            self.create_user()
+        self.client.force_login(self.user, backend="iati_account_web.oidc.IATIAccountOIDCAuthBackend")
+        session = self.client.session
+        session["oidc_id_token"] = self.iati_oidc_id_token
+        session["oidc_id_token_expiration"] = self.iati_oidc_claims["exp"]
+        session["oidc_access_token"] = self.iati_oidc_access_token
+        session["oidc_state"] = self.iati_oidc_state
+        session["oidc_nonce"] = self.iati_oidc_nonce
+        session.save()
