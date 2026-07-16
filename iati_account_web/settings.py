284154b7e0ffd31ff0602902b4b381b2008cfebf
Simon K <6615834+simon-20@users.noreply.github.com>
2026-07-16 11:12:46 +0100

    fix(settings): set fallback lang to a served language

--- iati_account_web/settings.py
+++ iati_account_web/settings.py
@@ -272,7 +272,7 @@ AUTH_USER_MODEL = "account.IATIUser"
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en"
 
 TIME_ZONE = "UTC"
 
